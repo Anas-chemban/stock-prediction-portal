fd713a2c1d32dad8e6df3fc791eb8803027337b6
anaschemban786 <anaschemban7860@gmail.com>
2025-12-23 14:33:27 +0530

    registration page

--- frentend/src/App.jsx
+++ frentend/src/App.jsx
@@ -2,7 +2,12 @@ import { useState } from 'react'
 import './assets/css/style.css'
 import Header from './components/Header.jsx'
 import Main from './components/Main.jsx'
+import Login from './components/Login.jsx'
 import Footer from './components/Footer.jsx'
+import Register from './components/register.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+
 
 
 function App() {
@@ -10,10 +15,16 @@ function App() {
 
   return (
     <>
+    <BrowserRouter>
     <Header />
-    <Main />
+      <Routes >
+        <Route path='/' element={<Main />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login'  element={<Login />}/>
+      </Routes>
     <Footer />
-
+    </BrowserRouter>
+    {/* <Main />     */}
 
       
     </>
